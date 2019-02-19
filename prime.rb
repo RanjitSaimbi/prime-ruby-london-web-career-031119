7d2ac6503f3b03288f31af8cf35558764bfebbc2
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-19 13:01:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  array = []
+  factor = 2 
+  
+  while number > 1 
+   if number % factor === 2 
+     array.push(factor)
+     number /= factor
+    else 
+     factor += 1 
+    end
+  end 
+  
+  if array.length > 2 
+    return false 
+  else 
+    true 
+  end 
+end
